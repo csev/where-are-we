b674b0dd1b0a3dbb71fe25e55f204806d4900216
Charles Severance <csev@umich.edu>
2015-12-26 13:11:03 -0500

    Tweak

--- static/map.js
+++ static/map.js
@@ -27,7 +27,7 @@ function initialize_map(lat,lng) {
     var row = other_points[i];
     // if ( i < 3 ) { alert(row); }
     var newLatlng = new google.maps.LatLng(row[0], row[1]);
-    var iconpath = '<?php echo($CFG->staticroot); ?>/static/img/icons/';
+    var iconpath = '/static/img/icons/';
     console.log(row);
     var icon = row[3] ? 'green-dot.png' : 'green.png';
     var marker = new google.maps.Marker({
