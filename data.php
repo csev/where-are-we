3c26c8b175277a4066d2e28173c838224b0c5a81
Charles Severance <csev@umich.edu>
2015-12-27 12:28:42 -0500

    Switch to trailing locations

--- data.php
+++ data.php
@@ -24,16 +24,37 @@ $stmt = $pdo->prepare('INSERT INTO user
     ON DUPLICATE KEY UPDATE login_at = NOW()');
 $stmt->execute(array( ':sid' => $user_key ));
 
-$stmt = $pdo->prepare('SELECT user_id FROM user WHERE user_sha256 = :sid');
+$stmt = $pdo->prepare('SELECT user.user_id,lat0,lng0,when0 
+    FROM user LEFT JOIN context_map ON context_map.user_id = user.user_id
+    WHERE user_sha256 = :sid');
 $stmt->execute(array( ':sid' => $user_key ));
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 $user_id = $row['user_id'];
 
-$stmt = $pdo->prepare('INSERT INTO context_map 
-   ( context_id, user_id, lat0, lng0, when0, updated_at )
-   VALUES ( 1, :uid, :lat, :lng, NOW(), NOW() )
-   ON DUPLICATE KEY UPDATE lat0=:lat,lng0=:lng,when0=NOW(),updated_at=NOW()');
-$stmt->execute(array( ':uid' => $user_id, ':lat' => $lat, ':lng' => $lng ));
+if ( is_null($row['lat0']) ) {
+    $delta = 0.0;
+} else {
+    $delta = abs($row['lat0']-$lat) + abs($row['lng0']-$lng);
+}
+
+if ( $delta < 0.002 ) {
+    $stmt = $pdo->prepare('INSERT INTO context_map 
+        ( context_id, user_id, lat0, lng0, when0, updated_at )
+        VALUES ( 1, :uid, :lat, :lng, NOW(), NOW() )
+        ON DUPLICATE KEY UPDATE lat0=:lat,lng0=:lng,when0=NOW(),updated_at=NOW()');
+    $stmt->execute(array( ':uid' => $user_id, ':lat' => $lat, ':lng' => $lng ));
+} else { 
+    $stmt = $pdo->prepare('INSERT INTO context_map 
+        ( context_id, user_id, lat0, lng0, when0, updated_at )
+        VALUES ( 1, :uid, :lat, :lng, NOW(), NOW() )
+        ON DUPLICATE KEY UPDATE 
+            lat3=lat2, lng3=lng2, when3=when2,
+            lat2=lat1, lng2=lng1, when2=when1,
+            lat1=lat0, lng1=lng0, when1=when0,
+            lat0=:lat, lng0=:lng, when0=NOW(),
+            updated_at=NOW()');
+    $stmt->execute(array( ':uid' => $user_id, ':lat' => $lat, ':lng' => $lng ));
+}
 
 
 $stmt = $pdo->prepare('SELECT displayname,email,lat0,lng0
